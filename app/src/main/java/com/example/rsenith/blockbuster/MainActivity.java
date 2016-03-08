8da4409102367fc983dcc029fced748fe76419b4
Senith <rsenith@gmail.com>
2016-03-08 00:25:53 -0800

    Initial commit

---
+++ app/src/main/java/com/example/rsenith/blockbuster/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.rsenith.blockbuster;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        ImageAdapter mMovieAdapter;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            mMovieAdapter =
+                    new ImageAdapter(
+                            getActivity()
+                            );
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            GridView gridview = (GridView)rootView.findViewById(R.id.gridview_movie);
+            gridview.setAdapter(mMovieAdapter);
+
+            return rootView;
+
+        }
+
+        public class ImageAdapter extends BaseAdapter {
+            private Context mContext;
+
+            public ImageAdapter(Context c) {
+                mContext = c;
+            }
+
+            public int getCount() {
+                return mThumbIds.length;
+            }
+
+            public Object getItem(int position) {
+                return null;
+            }
+
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            // create a new ImageView for each item referenced by the Adapter
+            public View getView(int position, View convertView, ViewGroup parent) {
+                ImageView imageView;
+                if (convertView == null) {
+                    // if it's not recycled, initialize some attributes
+                    imageView = new ImageView(mContext);
+                    imageView.setLayoutParams(new GridView.LayoutParams(540, 800));
+                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                } else {
+                    imageView = (ImageView) convertView;
+                }
+
+                imageView.setImageResource(mThumbIds[position]);
+                return imageView;
+            }
+
+            // references to our images
+            private Integer[] mThumbIds = {
+                    R.drawable.s1, R.drawable.s2,
+                    R.drawable.s3, R.drawable.s4,
+                    R.drawable.s5, R.drawable.s6,
+                    R.drawable.s7, R.drawable.s8
+            };
+        }
+
+
+
+
+    }
+}
